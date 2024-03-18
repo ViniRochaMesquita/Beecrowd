8dd4d460590166276926c3be294f717876b78c02
Vini Rocha <marcos646362@gmail.com>
2024-03-18 09:42:14 -0300

    1961_Pula Sapo Resolvido em C++

--- C++/1_Iniciante/1961_Pula Sapo/main.cpp
+++ C++/1_Iniciante/1961_Pula Sapo/main.cpp
@@ -1,10 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-
 int main() {
-
-
-
+  // P:Altura do pulo do Sapo;
+  // N:Número de canos;
+  int P=0, N=0,altura=0,cano_N=0,ganhou=1;
+  
+  cin >> P >> N;
+  //Leitura do primeiro cano
+  cin >> cano_N;
+  altura = cano_N;
+  
+  for(int i=0; i<N-1; i++){
+      if(ganhou)cin >> cano_N;
+      //Não existe distância negativa
+      if(abs(cano_N-altura)> P)ganhou = 0;
+      else altura = cano_N;
+  }
+  if(ganhou) cout << "YOU WIN" << endl;
+  else cout << "GAME OVER" << endl;
+  
   return 0;
 }
